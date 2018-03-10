053df5671bab5ad09de0b5108a36520724ec2e32
Eva Sarafianou <eva.sarafianou@gmail.com>
2018-03-10 21:03:21 +0800

    refactor html code so that a solution and the themes and solutions list appear in the center

--- src/frontend/js/components/SolutionsList.js
+++ src/frontend/js/components/SolutionsList.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { Link } from 'found'
 import { graphql, createFragmentContainer } from 'react-relay'
 import { withStyles } from 'material-ui/styles'
-import { List, ListItem } from 'material-ui'
+import { List, ListItem, Grid, Paper, Typography, Divider } from 'material-ui'
 import SolutionData from './SolutionData'
 
 const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30
+  },
+  paper: {
+    padding: 16,
+    color: theme.palette.text.secondary,
+    width: 1000
+  },
   link: {
     textDecoration: 'none',
     color: 'blue'
+  },
+  text: {
+    marginTop: 10,
+    marginBottom: 10
   }
 })
 
@@ -17,29 +30,35 @@ class SolutionsList extends React.Component {
     return solutions.map((solution, i) => {
       let solutionLink = '/solutions/' + solution.id
       return (
-        <ListItem key={i}>
-          <List>
-            <ListItem>Given voice: {solution.theme.given_voice}</ListItem>
-            <ListItem>
-              <SolutionData solution={solution} themeData={solution.theme.theme_data} givenVoice={solution.theme.given_voice} />
-            </ListItem>
-            <Link to={solutionLink} className={this.props.classes.link}>Continue editing</Link>
-          </List>
-        </ListItem>
+        <div key={i}>
+          <Divider />
+          <ListItem>
+            <List>
+              <ListItem>Given voice: {solution.theme.given_voice}</ListItem>
+              <ListItem>
+                <SolutionData solution={solution} themeData={solution.theme.theme_data} givenVoice={solution.theme.given_voice} />
+              </ListItem>
+              <Link to={solutionLink} className={this.props.classes.link}>Continue editing</Link>
+            </List>
+          </ListItem>
+        </div>
       )
     })
   }
 
   render () {
+    const { classes } = this.props
     return (
-      <div>
-        <header>
-          <h1> My Solutions </h1>
-        </header>
-        <List>
-          { this.createSolutionsList(this.props.solutions) }
-        </List>
-      </div>
+      <Grid justify='center' spacing={24} container className={classes.root}>
+        <Paper className={classes.paper}>
+          <Typography type='title' className={classes.text}>
+            My Solutions
+          </Typography>
+          <List>
+            { this.createSolutionsList(this.props.solutions) }
+          </List>
+        </Paper>
+      </Grid>
     )
   }
 }
--- src/frontend/js/components/ThemesList.js
+++ src/frontend/js/components/ThemesList.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Link } from 'found'
 import { graphql, createFragmentContainer } from 'react-relay'
-import { List, ListItem, Button, Divider } from 'material-ui'
+import { List, ListItem, Button, Divider, Grid, Paper, Typography } from 'material-ui'
 import { withStyles } from 'material-ui/styles'
 import ThemeData from './ThemeData'
 import createSolutionMutation from './createSolutionMutation'
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
+    flexGrow: 1,
+    marginTop: 30
+  },
+  paper: {
+    padding: 16,
+    color: theme.palette.text.secondary,
+    width: 1000
   },
   link: {
     textDecoration: 'none',
     color: 'blue'
+  },
+  text: {
+    marginTop: 20,
+    marginBottom: 5
   }
 })
 
@@ -24,18 +32,27 @@ class ThemesList extends React.Component {
   }
 
   createthemesList (themes) {
+    let difficulty = {
+      1: 'very easy',
+      2: 'easy',
+      3: 'medium',
+      4: 'advanced',
+      5: 'expert'
+    }
+
     return themes.map((theme, i) => {
       return (
         <div key={i}>
           <Divider />
           <ListItem>
             <List>
-              <ListItem key='1'>Given voice: {theme.given_voice}</ListItem>
-              <ListItem key='2'>{theme.description}</ListItem>
-              <ListItem key='3'><ThemeData theme={theme} /></ListItem>
+              <ListItem key='1'>{theme.description}</ListItem>
+              <ListItem key='2'>Given voice: {theme.given_voice}</ListItem>
+              <ListItem key='3'>Difficulty: {difficulty[theme.difficulty]}</ListItem>
+              <ListItem key='4'><ThemeData theme={theme} /></ListItem>
               {this.props.loggedIn
-              ? <Button raised key='4' onClick={() => { this.createSolution(theme) }}>Solve it!</Button>
-              : <Link to='/login' key='4' className={this.props.classes.link}>Login to start solving!</Link>
+              ? <Button raised key='5' onClick={() => { this.createSolution(theme) }}>Solve it!</Button>
+              : <Link to='/login' key='5' className={this.props.classes.link}>Login to start solving!</Link>
               }
             </List>
           </ListItem>
@@ -45,18 +62,21 @@ class ThemesList extends React.Component {
   }
 
   render () {
+    const { classes } = this.props
     return (
-      <div>
-        <header>
-          <h1>Themes</h1>
-        </header>
-        <div>Choose a theme and start solving its music harmony</div>
-        <div>
+      <Grid justify='center' spacing={24} container className={classes.root}>
+        <Paper className={classes.paper}>
+          <Typography type='title'>
+            Themes
+          </Typography>
+          <Typography type='subheading' className={classes.text}>
+            Choose a theme and start solving its music harmony
+          </Typography>
           <List className={this.props.classes.root}>
             { this.createthemesList(this.props.themes) }
           </List>
-        </div>
-      </div>
+        </Paper>
+      </Grid>
     )
   }
 }
