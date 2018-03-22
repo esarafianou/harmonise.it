d65f4e4e4926f228c390e76dd0d5fc2c606fde75
Eva Sarafianou <eva.sarafianou@gmail.com>
2018-03-21 19:10:26 -0700

    fix nit errors

--- src/frontend/js/utils.js
+++ src/frontend/js/utils.js
@@ -3,5 +3,5 @@ export const difficulty = {
   2: {name: 'EASY', color: '#78D29B'},
   3: {name: 'MEDIUM', color: '#F4DC77'},
   4: {name: 'ADVANCED', color: '#E97B3F'},
-  5: {name: 'EXPERT', color: 'E97B3F'}
+  5: {name: 'EXPERT', color: '#E97B3F'}
 }
