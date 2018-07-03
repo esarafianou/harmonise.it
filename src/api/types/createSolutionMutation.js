a4af7116ee7d8ce233f9c0bef806faafaec33763
Eva Sarafianou <eva.sarafianou@gmail.com>
2018-07-03 14:55:19 -0700

    remove accidentals for pauses and adjust their height within pentagram

--- src/api/types/createSolutionMutation.js
+++ src/api/types/createSolutionMutation.js
@@ -3,15 +3,30 @@ import { mutationWithClientMutationId, fromGlobalId } from 'graphql-relay'
 import { solutionType } from './solutionType'
 import { Theme, ThemeSolution } from '../../database.js'
 
+const pauseHeightAdjustment = {
+  soprano: {
+    0: 'g/4',
+    1: 'f/4',
+    2: 'e/3'
+  },
+  bass: {
+    0: 'c/5',
+    1: 'f/4',
+    2: 'e/4'
+  }
+}
+
 const _createSolutionData = (theme) => {
   let voices = []
   let note
   let themeData = JSON.parse(theme.theme_data)
-  for (let i = 0; i <= 3; i++) {
+  for (let i = 0; i < 3; i++) {
     let currentVoice = []
     for (let j = 0; j < themeData.voice.length; j++) {
       note = Object.assign({}, themeData.voice[j])
+      note.key = pauseHeightAdjustment[theme.given_voice][i]
       note.type = 'pause'
+      note.accidental = ''
       if (theme.given_voice === 'bass') {
         note.hint = ''
         note.rank = null
