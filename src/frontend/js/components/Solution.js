a009f09fcec7db3217bb46b518e85c39cfbf8252
Eva Sarafianou <eva.sarafianou@gmail.com>
2018-07-25 21:36:59 -0700

    present a message instead of an empty page when there is no solution

--- src/frontend/js/components/Solution.js
+++ src/frontend/js/components/Solution.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, createFragmentContainer } from 'react-relay'
 import { Grid, Paper, Typography, Chip } from 'material-ui'
 import { withStyles } from 'material-ui/styles'
+import { Link } from 'found'
 import { difficulty } from '../utils'
 import SolutionData from './SolutionData'
 
@@ -14,31 +15,45 @@ const styles = theme => ({
     padding: 16,
     color: theme.palette.text.secondary,
     width: 1000
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'blue',
+    marginBottom: 10
   }
 })
 
 class Solution extends React.Component {
   createSolutionDetails (solution) {
-    const chipStyle = {
-      backgroundColor: difficulty[solution.theme.difficulty].color,
-      color: 'white',
-      fontWeight: 'bold',
-      float: 'right'
+    if (solution) {
+      const chipStyle = {
+        backgroundColor: difficulty[solution.theme.difficulty].color,
+        color: 'white',
+        fontWeight: 'bold',
+        float: 'right'
+      }
+      return (
+        <Grid container>
+          <Grid item md={10}>
+            <Typography type='body2'>{solution.theme.description}</Typography>
+          </Grid>
+          <Grid item md={2}>
+            <Chip label={difficulty[solution.theme.difficulty].name} style={chipStyle} />
+          </Grid>
+          <Grid item md={12}>
+            <SolutionData solution={solution} themeData={solution.theme.theme_data} id={solution.id}
+              givenVoice={solution.theme.given_voice} router={this.props.router} editable />
+          </Grid>
+        </Grid>
+      )
+    } else {
+      return (
+        <Typography type='body1'>No solution matches with this link. Find all of your solutions
+          <Link to='/solutions' className={this.props.classes.link}> here</Link>!
+        </Typography>
+      )
     }
-    return (
-      <Grid container>
-        <Grid item md={10}>
-          <Typography type='body2'>{solution.theme.description}</Typography>
-        </Grid>
-        <Grid item md={2}>
-          <Chip label={difficulty[solution.theme.difficulty].name} style={chipStyle} />
-        </Grid>
-        <Grid item md={12}>
-          <SolutionData solution={solution} themeData={solution.theme.theme_data} id={solution.id}
-            givenVoice={solution.theme.given_voice} router={this.props.router} editable />
-        </Grid>
-      </Grid>
-    )
+  }
 
   componentDidMount () {
     if (!this.props.loggedIn) {
